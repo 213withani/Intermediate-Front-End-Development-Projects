345e145c1d366cdc3fc890d5898bb3b663a60743
Israel <213withani@gmail.com>
2017-09-06 19:56:58 -0700

    refactored

--- Show_the_Local_Weather.js
+++ Show_the_Local_Weather.js
@@ -1,44 +1,54 @@
 $(document).ready(function() {
 
   var lat, lon;
-  var apiURL="";
-
-  function toFahrenheit(deg){
-    var num=1.8 * (deg - 273) + 32;
-    return num.toFixed();
-  }
-
-  function toCelcius(deg){
-    var num =deg-273;
-    return num.toFixed();
-  }
-
-  function success(position) {
-          lat=position.coords.latitude.toFixed();
-          lon=position.coords.longitude.toFixed();
-
-          apiURL="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=2ed49d05c64144ca0d7f84a62aa20fdb";
-
-          $.getJSON(apiURL, function(json) {
-
-            $(".celcius").on('click', function (){
-              var icon="<img src=\"https://openweathermap.org/img/w/"+json.weather[0].icon+".png\"</img>";
-              // $(".weather").html(icon);
-              console.log(icon);
-            $(".weather").html('Icon: '+icon+ '<br/>Weather: '+json.weather[0].main+ '<br/>Temperature: '+ toCelcius(json.main.temp)+'<br/>Country: '+json.sys.country+' <br/>Name: '+json.name);
-            });
-
-            $(".fahrenheit").on('click', function (){
-              $(".weather").html('Icon: '+icon+ '<br/>Weather: '+json.weather[0].main+ '<br/>Temperature: '+ toFahrenheit(json.main.temp)+'<br/>Country: '+json.sys.country+' <br/>Name: '+json.name);
-            });
-
-          });
+  var apiURL = "";
 
 
+  function location(position) {
+    lat = position.coords.latitude.toFixed();
+    lon = position.coords.longitude.toFixed();
+
+    apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=2ed49d05c64144ca0d7f84a62aa20fdb";
+
+    function weatherData(json) {
+      var icon = "<img src=\"https://openweathermap.org/img/w/" + json.weather[0].icon + ".png\"</img>";
+      var temp = json.main.temp;
+      var mainWeather = json.weather[0].main;
+      var country = json.sys.country;
+      var loc = json.name;
+
+      $("#celcius").on("click", function() {
+
+        var displayInfo = 'Icon: ' + icon + '<br/>Weather: ' + mainWeather + '<br/>Temperature: ' +
+          toCelcius(temp) + '<br/>Country: ' + country + ' <br/>Name: ' + loc;
+
+        function toCelcius(deg) {
+          var num = deg - 273;
+          return num.toFixed();
+        }
+
+        $(".weather").html(displayInfo);
+      });
+
+      $("#fahrenheit").on("click", function() {
+
+        var displayInfo = 'Icon: ' + icon + '<br/>Weather: ' + mainWeather + '<br/>Temperature: ' +
+          toFahrenheit(temp) + '<br/>Country: ' + country + ' <br/>Name: ' + loc;
+
+        function toFahrenheit(deg) {
+          var num = 1.8 * (deg - 273) + 32;
+          return num.toFixed();
+        }
+
+        $(".weather").html(displayInfo);
+      });
     }
 
+    $.getJSON(apiURL, weatherData);
+  }
+
   if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success);
+    navigator.geolocation.getCurrentPosition(location);
   }
 
 });
