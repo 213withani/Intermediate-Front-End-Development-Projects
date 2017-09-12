28275faba502cb9a8a5a269e4fa8f1837d3c88a5
Israel <213withani@gmail.com>
2017-09-11 21:53:12 -0700

    I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.

--- Build_a_Wikipedia_Viewer.js
+++ Build_a_Wikipedia_Viewer.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       var apiURL="";
       var result=$(".result");
       var output="";
-
+      console.log(value);
         $.ajax( {
             url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search="+value,
             dataType: 'json',
@@ -13,12 +13,31 @@ $(document).ready(function(){
             headers: { 'Api-User-Agent': 'Example/1.0' },
             success: function(searchResults) {
               var max=10;
+              var textLink="",description="",hrefInfo="";
+
               for(let child=0;child<max;child++){
 
                     for(let parent=1;parent<searchResults.length;parent++){
-                        output+=searchResults[parent][child]+'<br/>';
+                      // go through the first and create a link with empty href
+                      // attach description
+                      // add href to links
+
+                        //output+=searchResults[parent][child]+'<br/>';
+                      if (parent===1){
+                        textLink=searchResults[parent][child];
+                      }
+                      if (parent===2){
+                        description=searchResults[parent][child];
+                      }
+                      if (parent===3){
+                        hrefInfo=searchResults[parent][child];
+                      }
+
                     }
-                    output+='<br/>';
+                    textLink="<a target='_blank' href='"+hrefInfo+"'>"+textLink+"</a>"
+                    //console.log(textLink);
+                    output+=textLink+'<br/>'+description+'<br/><br/>';
+
               }
               result.html(output);
             }
