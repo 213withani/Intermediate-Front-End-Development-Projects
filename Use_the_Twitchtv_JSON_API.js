c0f530810886d5f8fd77ccde06b16b4f8727e82d
Israel <213withani@gmail.com>
2017-09-13 22:36:12 -0700

    I can see whether Free Code Camp is currently streaming on Twitch.tv. I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel

--- Use_the_Twitchtv_JSON_API.js
+++ Use_the_Twitchtv_JSON_API.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
       var user=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp"];
-      var apiURL="",apiURL2="";
+      var apiURL="";
 
       for(var i=0; i<user.length;i++){
-            apiURL="https://wind-bow.gomix.me/twitch-api/channels/"+user[i]+"?callback=?";
-            $.getJSON(apiURL, function(data) {
-              $(".channels").append("<a href='"+data.url+"'> <img src='"+data.logo+"'> </a><br/><br/>");
-            });
 
-            apiURL2="https://wind-bow.gomix.me/twitch-api/streams/"+user[i]+"?callback=?";
-            $.getJSON(apiURL2, function(data) {
-            var userStatus="NA";
+            apiURL="https://wind-bow.gomix.me/twitch-api/streams/"+user[i]+"?callback=?";
+            $.getJSON(apiURL, function(data) {
+            var userStatus="NA", logo="",display_name="",status="",url="";
             var viewers=0;
             var id=0;
 
@@ -18,13 +14,19 @@ $(document).ready(function() {
                   userStatus=data.stream.stream_type;
                   viewers=data.stream.viewers;
                   id=data.stream._id;
+                  logo=data.stream.channel.logo;
+                  display_name=data.stream.channel.display_name;
+                  status=data.stream.channel.status;
+                  url=data.stream.channel.url;
                 }else{
                   userStatus="Offline";
                   viewers=0;
-                  id=0;
+                  logo="https://orels1.tips/s/twitch-offline-no-text.jpg";
+
                 }
 
-                $(".data").append("<br/>ID: "+id+ " Status: "+ userStatus +" Viewers:"+ viewers+" links: "+data._links.channel);
+                //$(".data").append("<br/> Status: "+ userStatus +" Viewers:"+ viewers+" links: "+data._links.channel+data.stream.channel.status);
+            $(".data").append("<br/><a target='_blank' href='"+url+"'>"+"<img width=300 height=300 src='"+logo+"'></a> Channel: "+display_name+" Status: "+ userStatus +" Viewers:"+ viewers+" Status: "+status+"<br/>");
           });
       }
 
